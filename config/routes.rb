55fbcd97920712a474e0703c97f921624263a5a2
Nelson Blaha <nelson.blaha@gmail.com>
2013-01-13 16:02:58 -0600

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,7 @@ Boilerplate::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
